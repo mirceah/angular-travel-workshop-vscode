7137743b8908156426845606f081ddc1f9aa0caf
Mircea Hasegan <mircea.hasegan@gmail.com>
2020-04-26 17:57:51 +0300

    4. Directive Structurale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'meTravel';
 
-  destination = {
+  destinations = [
+    {
       name: 'Minas Tirith',
       summary: 'Fortified capital of the kingdom of Gondor',
       description: 'Minas Tirith covered the entire Hill of Guard. You will see well preserved weapon chambers , climb through the seven levels all the way up to the Citadel where you can feast at the lovely Isildur restaurant, just like kings used to',
       price: 4800,
       isFavorite: false,
-    };
+    },
+    {
+      name: 'Mordor',
+      summary: 'Realm and base of operations of Sauron',
+      description: 'Visit the once feared land of Mordor. A one day visit to the Sauron Museum, followed by lovely hiking on the three enormous mountain ranges surrounding it and last but not least, the Orc Catacombs where many mumified Orc bodies can be observed',
+      price: 5000,
+      isFavorite: false,
+    }
+  ];
+
+  destination = this.destinations[0]
 }
