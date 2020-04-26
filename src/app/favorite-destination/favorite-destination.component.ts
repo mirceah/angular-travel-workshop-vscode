d0268dc2e583a82969e9fd069df6be943cf1ebaf
Mircea Hasegan <mircea.hasegan@gmail.com>
2020-04-26 19:54:09 +0300

    7. Components

---
+++ src/app/favorite-destination/favorite-destination.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-favorite-destination',
+//   templateUrl: './favorite-destination.component.html',
+//   styleUrls: ['./favorite-destination.component.css']
+// })
+export class FavoriteDestinationComponent {
+  
+}
